5c89c356093e3621a9d1b51c5d8595f9fa0429ff
Steve Greenberg <sgreenberg@rscale.io>
2022-09-27 12:22:54 -0600

    domain mapping for cloud run

---
+++ gcp/cloud-run-domain-mapping/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53, < 5.0"
+    }
+  }
+  backend "gcs" {}
+  required_version = ">= 0.13"
+}
+
+provider "google" {
+  project = var.gcp_project_id
+}
+
+variable "project" {
+  type = "string"
+}
+
+variable "dns_zone_name" {
+  type = "string"
+}
+
+variable "cloud_run_domain" {
+  type = "string"
+}
+
+variable "cloud_run_location" {
+  type = "string"
+}
+
+variable "cloud_run_service" {
+  type = "string"
+}
+
+data "google_dns_managed_zone" "zone" {
+  name = var.dns_zone_name
+}
+
+data "google_cloud_run_service" "crs" {
+  name     = var.cloud_run_service
+  location = var.cloud_run_location
+}
+
+resource "google_dns_record_set" "a" {
+  name         = "${var.cloud_run_domain}."
+  managed_zone = data.google_dns_managed_zone.zone.name
+  type         = "A"
+  ttl          = 300
+}
+
+resource "google_cloud_run_domain_mapping" "crdm" {
+  location = var.cloud_run_location
+  name     = var.cloud_run_domain
+
+  spec {
+    route_name = google_cloud_run_service.crs.name
+  }
+}
