0d9be1cd4c9ed6ae7107b661404043c95cbb9689
Steve Greenberg <sgreenberg@rscale.io>
2022-09-28 16:56:21 -0600

    access first resource record

--- gcp/cloud-run-domain-mapping/main.tf
+++ gcp/cloud-run-domain-mapping/main.tf
@@ -77,6 +77,6 @@ resource "google_dns_record_set" "cname" {
   managed_zone = data.google_dns_managed_zone.zone.name
   type         = "CNAME"
   ttl          = 300
-  rrdatas = [google_cloud_run_domain_mapping.crdm.status.resource_records.rrdata]
+  rrdatas = [google_cloud_run_domain_mapping.crdm.status.resource_records[0].rrdata]
 
 }
